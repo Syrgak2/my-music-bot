97643e87cc8e4dfaa360e911835ba554c7c6b1b1
Syrgak <Kary6ekov.s@gmail.com>
2024-03-06 20:09:22 +0100

    created bot skeleton

---
+++ src/main/java/com/example/my/music/bot/MyMusicBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.my.music.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMusicBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMusicBotApplication.class, args);
+	}
+
+}
